23b544a608033b4e131058eacce9fe26e8609a6e
Rodricasares <rodrigocantudo@gmail.com>
2022-05-30 14:51:35 +0000

    Sin acaabar aun

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -21,25 +22,102 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+# hello API
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response"
     }
+    return jsonify(response_body)
 
-    return jsonify(response_body), 200
+
+# Listar todos los usuarios del blog
+
+@app.route('/user', methods=['GET'])
+def list_users():
+    user = User.query.all()
+    userList_serialized = [userList.serialize() for userList in user]
+
+    return jsonify({'response': userList_serialized})
+
+#listar un solo user
+@app.route('/user/<int:id_user>', methods=['GET'])
+def get_user(id_user):
+    user = User.query.filter_by(id=id_user).first()
+    user = User.query.get(id_user)
+    return jsonify(user.serialize()), 200
+
+# Agregar usuarios
+
+@app.route("/user", methods=['POST']) # aquí especificamos que estos endpoints aceptan solicitudes POST y GET.
+def add_user():
+    request_data = request.data
+    data = json.loads(request_data)
+    user.append(data)
+    return jsonify({"msg" : "Usuario creado correctamente"})
+
+# Borrar user
+
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+def delete_user(id_user):
+    user.pop(id_user)
+    return jsonify({"msg" : "Usuario eliminado correctamente"})
+
+#Listar la información de los favoritos
+
+@app.route('/favorite', methods=['GET'])
+def list_favorite():
+    favorite = Favorite.query.all()
+    fav_serialized = [fav.serialize() for fav in favorite]
+
+    return jsonify({'response':fav_serialized})
+
+#Listar la información de todos los planetas
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planets.query.all()
+  # planets_serialized = list(map(lambda planets:planets.serialize(), planets))
+    planets_serialized = [planet.serialize() for planet in planets]
+
+    return jsonify({'response':planets_serialized})
+
+#Listar la información de un solo planet
+
+@app.route('/planets/<int:id_planets>', methods=['GET'])
+def get_planets(id_planets):
+    planet = Planets.query.filter_by(id=id_planets).first()
+    planet = Planets.query.get(id_planets)
+    return jsonify(planet.serialize()), 200
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
+
+
+
+
+
+   # planet = {}
+   # for planet in planets:
+    #    if item.get(id) == id_planets:
+     #       planet = item
+    #return jsonify(palnet)
--- src/models.py
+++ src/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref = 'user')
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +17,52 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username
+            "planets_id": Planets.query.get(self.planets_id).serialize(),
             # do not serialize the password, its a security breach
         }
+
+class Favorite(db.Model):   
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(Integer, ForeignKey('user.id'))
+    planets_id = db.Column(Integer, ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": Planets.query.get(self.planets_id).serialize(),
+        }
+        
+class Planets(db.Model):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    img = db.Column(db.String(120), nullable=False, unique=True)
+    climate = db.Column(db.String(120), nullable=False, unique=True)
+    gravity = db.Column(db.String(120), nullable=False, unique=True)
+    terrain = db.Column(db.String(120), nullable=False, unique=True)
+    population = db.Column(db.String(120), nullable=False, unique=True)
+    orbital_period = db.Column(db.String(120), nullable=False, unique=True)
+    rotation_period = db.Column(db.String(120), nullable=False, unique=True)
+    favorites_id = db.relationship('Favorite', backref = 'planets')
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "img": self.img,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "population": self.population,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period
+          }
+          
